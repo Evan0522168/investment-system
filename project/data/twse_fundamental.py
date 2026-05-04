b75ea3329aff2189b84d8631e4b51a8fd0f2c846
Evan0522168 <yuche0522@gmail.com>
2026-05-04 14:57:36 +0800

    Update twse_fundamental.py

--- project/data/twse_fundamental.py
+++ project/data/twse_fundamental.py
@@ -1,17 +1,15 @@
 import requests
 import pandas as pd
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def fetch_twse_fundamental():
-
     url = "https://openapi.twse.com.tw/v1/exchangeReport/BWIBBU_ALL"
-
-    r = requests.get(url)
-
+    r = requests.get(url, verify=False)
     data = r.json()
-
     df = pd.DataFrame(data)
-
     df.rename(columns={
         "Code": "Symbol",
         "Name": "Name",
@@ -19,5 +17,4 @@ def fetch_twse_fundamental():
         "DividendYield": "DividendYield",
         "PBratio": "PB"
     }, inplace=True)
-
     return df
