8402365c9f9ef7bc04cb628578e540cacb4c016c
Evan0522168 <yuche0522@gmail.com>
2026-05-04 14:57:58 +0800

    Update twse_list.py

--- project/data/twse_list.py
+++ project/data/twse_list.py
@@ -1,20 +1,17 @@
 import requests
 import pandas as pd
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def get_twse_stock_list():
-
     url = "https://openapi.twse.com.tw/v1/exchangeReport/STOCK_DAY_ALL"
-
-    r = requests.get(url)
-
+    r = requests.get(url, verify=False)
     data = r.json()
-
     df = pd.DataFrame(data)
-
     df.rename(columns={
         "Code": "Symbol",
         "Name": "Name"
     }, inplace=True)
-
     return df[["Symbol", "Name"]]
